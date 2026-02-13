bccfef2245c710595209843060f5c79d82e19b43
SinZ <osk0121@naver.com>
2026-02-13 19:03:46 +0900

    init

---
+++ java/me/sinz/sinz/MainActivity.java
@@ -0,0 +1,13 @@
+package me.sinz.sinz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
